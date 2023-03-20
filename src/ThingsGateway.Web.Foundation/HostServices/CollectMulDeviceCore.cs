e9fe0992c64745f07705caaecf51cbf426c81199
2248356998 qq.com <2248356998@qq.com>
2023-03-20 13:34:59 +0800

    更新nuget，暂缓链路复用功能

---
+++ src/ThingsGateway.Web.Foundation/HostServices/CollectMulDeviceCore.cs
@@ -0,0 +1,25 @@
+﻿namespace ThingsGateway.Web.Foundation;
+
+
+public class CollectMulDeviceCore : CollectDeviceCore
+{
+    public CollectMulDeviceCore(IServiceScopeFactory scopeFactory):base(scopeFactory)
+    {
+
+    }
+    
+
+
+    public override void StartThread()
+    {
+        DeviceTask?.Start();
+    }
+    public override void StopThread()
+    {
+    }
+
+
+
+
+}
+
