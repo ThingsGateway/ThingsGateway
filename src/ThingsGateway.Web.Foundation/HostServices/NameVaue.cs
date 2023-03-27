e44c0f85c2b1fb9806a74d8288c096bfc1fa10c0
2248356998 qq.com <2248356998@qq.com>
2023-03-27 19:01:04 +0800

    swagger文档

--- src/ThingsGateway.Web.Foundation/HostServices/NameVaue.cs
+++ src/ThingsGateway.Web.Foundation/HostServices/NameVaue.cs
@@ -2,6 +2,12 @@
 
 public class NameVaue
 {
+    /// <summary>
+    /// 变量名称
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// 变量值
+    /// </summary>
     public string Value { get; set; }
 }
