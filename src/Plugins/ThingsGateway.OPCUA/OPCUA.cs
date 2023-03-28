b765fa476953666f7b774184f29c1f9a7b414bd2
2248356998 qq.com <2248356998@qq.com>
2023-03-28 14:03:11 +0800

    添加写入采集时间的接口，而不是固定DateTime.Now

--- src/Plugins/ThingsGateway.OPCUA/OPCUA.cs
+++ src/Plugins/ThingsGateway.OPCUA/OPCUA.cs
@@ -168,11 +168,11 @@ namespace ThingsGateway.OPCUA
                         var time = data.monitoredItemNotification.Value.SourceTimestamp;
                         if (value != null && quality == 192)
                         {
-                            item.SetValue(value);
+                            item.SetValue(value, time);
                         }
                         else
                         {
-                            item.SetValue(null);
+                            item.SetValue(null, time);
                             Device.DeviceStatus = DeviceStatusEnum.OnLineButNoInitialValue;
                             Device.DeviceOffMsg = $"{item.Name} 质量为Bad ";
                         }
