31c37f41b20d98310dca6830d53f4059795fb205
2248356998 qq.com <2248356998@qq.com>
2023-03-30 13:53:57 +0800

    删除多余代码

--- src/ThingsGateway.Web.Foundation/Extension/ServiceExtension.cs
+++ src/ThingsGateway.Web.Foundation/Extension/ServiceExtension.cs
@@ -9,16 +9,16 @@ namespace ThingsGateway.Web.Foundation
     /// </summary>
     public static class ServiceExtension
     {
+        ///// <summary>
+        ///// 获取后台服务
+        ///// </summary>
+        //public static T GetBackgroundService<T>(this IServiceScopeFactory @this) where T : class, IHostedService
+        //{
+        //    var hostedService = @this.CreateScope().ServiceProvider.GetServices<IHostedService>().FirstOrDefault(it => it is T) as T;
+        //    return hostedService;
+        //}
         /// <summary>
-        /// 获取后台服务,用于非IHostService
-        /// </summary>
-        public static T GetBackgroundService<T>(this IServiceScopeFactory @this) where T : class, IHostedService
-        {
-            var hostedService = @this.CreateScope().ServiceProvider.GetServices<IHostedService>().FirstOrDefault(it => it is T) as T;
-            return hostedService;
-        }
-        /// <summary>
-        /// 获取后台服务，注意在后台上直接获取可能会出错
+        /// 获取后台服务
         /// </summary>
         public static T GetBackgroundService<T>(this object @this) where T : class, IHostedService
         {
