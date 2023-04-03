74634889ab4880639c07b3115ed39de58af6c51f
2248356998 qq.com <2248356998@qq.com>
2023-04-03 19:30:52 +0800

    添加IotSharp插件

---
+++ src/Plugins/ThingsGateway.Mqtt/IotSharp/RpcResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace IoTSharp.Data
+{
+    public class RpcResponse
+    {
+        public string DeviceId { get; set; }
+        public string Method { get; set; }
+        public string ResponseId { get; set; }
+        public string Data { get; set; }
+    }
+
+}
