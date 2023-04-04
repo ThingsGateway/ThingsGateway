af06755ada5e3af71c5fa21c96b0e8fe4520b0a5
2248356998 qq.com <2248356998@qq.com>
2023-04-04 09:18:58 +0800

    添加写入多个变量的api方法

--- src/ThingsGateway.Web.Foundation/Controllers/RpcControler.cs
+++ src/ThingsGateway.Web.Foundation/Controllers/RpcControler.cs
@@ -30,15 +30,29 @@ namespace ThingsGateway.Web.Foundation
 
         RpcCore _rpcCore { get; set; }
         /// <summary>
-        /// 写入设备
+        /// 写入变量
         /// </summary>
-        [HttpPost("writeDeviceMethod")]
-        [Description("写入设备")]
+        [HttpPost("writeVariable")]
+        [Description("写入变量")]
         public Task<OperResult> WriteDeviceMethod(NameValue obj)
         {
             return _rpcCore.InvokeDeviceMethod($"WebApi-{UserManager.UserAccount}-{App.HttpContext.Connection.RemoteIpAddress.MapToIPv4()}", obj);
         }
-
+        /// <summary>
+        /// 写入多个变量
+        /// </summary>
+        [HttpPost("writeVariables")]
+        [Description("写入多个变量")]
+        public async Task<Dictionary<string, OperResult>> WriteDeviceMethods(List<NameValue> objs)
+        {
+            Dictionary<string, OperResult> operResultDict = new Dictionary<string, OperResult>();
+            foreach (var obj in objs)
+            {
+                var result=await _rpcCore.InvokeDeviceMethod($"WebApi-{UserManager.UserAccount}-{App.HttpContext.Connection.RemoteIpAddress.MapToIPv4()}", obj);
+                operResultDict.Add(obj.Name, result);
+            }
+            return operResultDict;
+        }
     }
 }
 
