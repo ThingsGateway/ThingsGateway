315252bdc4e773dcd4969b2f6f6321320a0d30d6
2248356998 qq.com <2248356998@qq.com>
2023-04-24 17:54:00 +0800

    添加常用转换

--- src/ThingsGateway.Foundation/Foundation/StaticHelper/DataHelper.cs
+++ src/ThingsGateway.Foundation/Foundation/StaticHelper/DataHelper.cs
@@ -72,6 +72,59 @@ public class DataHelper
         offset = addressStart % 8;
         newStart = addressStart - offset;
     }
+
+    /// <summary>
+    /// 16进制Char转int
+    /// </summary>
+    /// <param name="ch"></param>
+    /// <returns></returns>
+    public static int GetIntByHexChar(char ch)
+    {
+        switch (ch)
+        {
+            case '0':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+            case 'A':
+            case 'a':
+                return 10;
+            case 'B':
+            case 'b':
+                return 11;
+            case 'C':
+            case 'c':
+                return 12;
+            case 'D':
+            case 'd':
+                return 13;
+            case 'E':
+            case 'e':
+                return 14;
+            case 'F':
+            case 'f':
+                return 15;
+            default:
+                return -1;
+        }
+    }
+
     /// <summary>
     /// 获取BCD值
     /// </summary>
