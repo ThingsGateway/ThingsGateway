6137e6baa506a492a99b34e37e4d750cdcdf0ffd
2248356998 qq.com <2248356998@qq.com>
2023-05-08 17:45:13 +0800

    add GetSciptValue

--- src/ThingsGateway.Web.Foundation/Extension/CSharpScriptEngineExtension.cs
+++ src/ThingsGateway.Web.Foundation/Extension/CSharpScriptEngineExtension.cs
@@ -30,6 +30,25 @@ namespace ThingsGateway.Web.Foundation
                 return inPut;
             }
         }
+
+        /// <summary>
+        /// 获取返回值
+        /// </summary>
+        public static string GetSciptValue<T>(this T datas, string script) where T : class
+        {
+            var inPut = datas.ToJson();
+            if (!script.IsNullOrEmpty())
+            {
+                //执行脚本，获取新实体
+                var outPut = _cSharpScriptEngine.Do(script, inPut);
+                return outPut;
+            }
+            else
+            {
+                return inPut;
+            }
+        }
+
     }
 }
 
