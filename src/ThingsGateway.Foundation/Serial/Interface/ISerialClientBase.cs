56b87fc1f58fcef758f16b1fd956f982dc937bb6
2248356998 qq.com <2248356998@qq.com>
2023-05-23 23:54:28 +0800

    Add copyright notices

--- src/ThingsGateway.Foundation/Serial/Interface/ISerialClientBase.cs
+++ src/ThingsGateway.Foundation/Serial/Interface/ISerialClientBase.cs
@@ -1,3 +1,15 @@
+#region copyright
+//------------------------------------------------------------------------------
+//  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
+//  此代码版权（除特别声明外的代码）归作者本人Diego所有
+//  源代码使用协议遵循本仓库的开源协议及附加协议
+//  Gitee源代码仓库：https://gitee.com/diego2098/ThingsGateway
+//  Github源代码仓库：https://github.com/kimdiego2098/ThingsGateway
+//  使用文档：https://diego2098.gitee.io/thingsgateway/
+//  QQ群：605534569
+//------------------------------------------------------------------------------
+#endregion
+
 using System.IO.Ports;
 
 namespace ThingsGateway.Foundation.Serial
