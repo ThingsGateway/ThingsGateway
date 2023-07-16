baca0a70c0234371756be3bbf7228c8e3b30daad
2248356998 qq.com <2248356998@qq.com>
2023-07-16 17:48:22 +0800

    更新文档

--- src/Plugins/ThingsGateway.Foundation.Adapter.OPCUA/OPCUAClient/OPCUAClient.cs
+++ src/Plugins/ThingsGateway.Foundation.Adapter.OPCUA/OPCUAClient/OPCUAClient.cs
@@ -3,9 +3,9 @@
 //  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
 //  此代码版权（除特别声明外的代码）归作者本人Diego所有
 //  源代码使用协议遵循本仓库的开源协议及附加协议
-//  Gitee源代码仓库：https://gitee.com/dotnetchina/ThingsGateway
+//  Gitee源代码仓库：https://gitee.com/diego2098/ThingsGateway
 //  Github源代码仓库：https://github.com/kimdiego2098/ThingsGateway
-//  使用文档：https://diego2098.gitee.io/thingsgateway/
+//  使用文档：https://diego2098.gitee.io/thingsgateway-docs/
 //  QQ群：605534569
 //------------------------------------------------------------------------------
 #endregion
@@ -98,7 +98,7 @@ public class OPCUAClient : DisposableObject
             ApplicationType = ApplicationType.Client,
             CertificateValidator = certificateValidator,
             ApplicationUri = Utils.Format(@"urn:{0}:thingsgatewayopcuaclient", System.Net.Dns.GetHostName()),
-            ProductUri = "https://diego2098.gitee.io/thingsgateway/",
+            ProductUri = "https://diego2098.gitee.io/thingsgateway-docs/",
 
             ServerConfiguration = new ServerConfiguration
             {
