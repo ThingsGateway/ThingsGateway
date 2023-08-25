4be3dcce5038b7a417e9aeb51eec428a84c2395e
Kimdiego2098 <2248356998@qq.com>
2023-08-25 19:33:08 +0800

    添加DLT645_2007协议插件

---
+++ framework/ThingsGateway.Plugin/Foundataion/ThingsGateway.Foundation.Adapter.DLT645/DLT645/DLT645_2007DataHandleAdapter.cs
@@ -0,0 +1,115 @@
+﻿#region copyright
+//------------------------------------------------------------------------------
+//  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
+//  此代码版权（除特别声明外的代码）归作者本人Diego所有
+//  源代码使用协议遵循本仓库的开源协议及附加协议
+//  Gitee源代码仓库：https://gitee.com/diego2098/ThingsGateway
+//  Github源代码仓库：https://github.com/kimdiego2098/ThingsGateway
+//  使用文档：https://diego2098.gitee.io/thingsgateway-docs/
+//  QQ群：605534569
+//------------------------------------------------------------------------------
+#endregion
+
+using System.ComponentModel;
+
+using ThingsGateway.Foundation.Adapter.Modbus;
+using ThingsGateway.Foundation.Extension.Generic;
+
+namespace ThingsGateway.Foundation.Adapter.DLT645;
+
+/// <summary>
+/// DLT645_2007DataHandleAdapter
+/// </summary>
+public class DLT645_2007DataHandleAdapter : ReadWriteDevicesTcpDataHandleAdapter<DLT645_2007Message>
+{
+    /// <summary>
+    /// 增加FE FE FE FE的报文头部
+    /// </summary>
+    [Description("前导符报文头")]
+    public bool EnableFEHead { get; set; }
+
+    /// <inheritdoc/>
+    public override byte[] PackCommand(byte[] command)
+    {
+        //打包时加上4个FE字节
+        if (EnableFEHead)
+        {
+            return DataTransUtil.SpliceArray(new byte[4] { 0xFE, 0xFE, 0xFE, 0xFE }, command);
+        }
+        return command;
+    }
+
+    /// <inheritdoc/>
+    protected override DLT645_2007Message GetInstance()
+    {
+        return new DLT645_2007Message();
+    }
+
+
+    /// <inheritdoc/>
+    protected override FilterResult UnpackResponse(DLT645_2007Message request, byte[] send, byte[] body, byte[] response)
+    {
+        //因为设备可能带有FE前导符开头，这里找到0x68的位置
+        int headCodeIndex = -1;
+        if (response != null)
+        {
+            for (int index = 0; index < response.Length; index++)
+            {
+                if (response[index] == 0x68)
+                {
+                    headCodeIndex = index;
+                    break;
+                }
+            }
+        }
+
+        //帧起始符 地址域  帧起始符 控制码 数据域长度共10个字节
+        if (headCodeIndex < 0 || headCodeIndex + 10 > response.Length)
+            return FilterResult.Cache;
+
+
+        var len = 10 + response[headCodeIndex + 9] + 2;
+
+        if (response.Length - headCodeIndex < len)
+        {
+            return FilterResult.Cache;
+        }
+        if (response.Length - headCodeIndex >= len && response[len + headCodeIndex - 1] == 0x16)
+        {
+
+            //检查校验码
+            int sumCheck = 0;
+            for (int i = headCodeIndex; i < len + headCodeIndex - 2; i++)
+                sumCheck += response[i];
+            if ((byte)sumCheck != response[len + headCodeIndex - 2])
+            {
+                //校验错误
+                request.Message = "和校验错误";
+                request.ResultCode = ResultCode.Fail;
+                return FilterResult.Success;
+            }
+            if ((response[headCodeIndex + 8] & 0x40) == 0x40)//控制码bit6为1时，返回错误
+            {
+                byte byte1 = (byte)(response[headCodeIndex + 10] - 0x33);
+                var error = DLT645Helper.Get2007ErrorMessage(byte1);
+                request.Message = "异常控制码：" + $"0x{response[headCodeIndex + 8]:X2}，错误信息：{error}";
+                request.ResultCode = ResultCode.Fail;
+                return FilterResult.Success;
+            }
+            else
+            {
+                request.Content = response.RemoveBegin(headCodeIndex + 10).RemoveLast(response.Length + 2 - len - headCodeIndex);
+                request.ResultCode = ResultCode.Success;
+                return FilterResult.Success;
+            }
+
+        }
+        else
+        {
+            request.ResultCode = ResultCode.Error;
+            return FilterResult.Success;
+
+        }
+    }
+
+}
---
+++ framework/ThingsGateway.Plugin/Foundataion/ThingsGateway.Foundation.Adapter.DLT645/DLT645/DLT645_2007OverTcp.cs
@@ -0,0 +1,416 @@
+﻿#region copyright
+//------------------------------------------------------------------------------
+//  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
+//  此代码版权（除特别声明外的代码）归作者本人Diego所有
+//  源代码使用协议遵循本仓库的开源协议及附加协议
+//  Gitee源代码仓库：https://gitee.com/diego2098/ThingsGateway
+//  Github源代码仓库：https://github.com/kimdiego2098/ThingsGateway
+//  使用文档：https://diego2098.gitee.io/thingsgateway-docs/
+//  QQ群：605534569
+//------------------------------------------------------------------------------
+#endregion
+
+using System.ComponentModel;
+
+using ThingsGateway.Foundation.Extension.Byte;
+using ThingsGateway.Foundation.Extension.Generic;
+
+namespace ThingsGateway.Foundation.Adapter.DLT645;
+/// <summary>
+/// DLT645_2007
+/// </summary>
+public class DLT645_2007OverTcp : ReadWriteDevicesTcpClientBase
+{
+
+    /// <summary>
+    /// DLT645_2007
+    /// </summary>
+    /// <param name="tcpClient"></param>
+    public DLT645_2007OverTcp(TcpClientEx tcpClient) : base(tcpClient)
+    {
+        ThingsGatewayBitConverter = new DLT645_2007BitConverter(EndianType.Big);
+        RegisterByteLength = 2;
+    }
+
+
+
+    /// <summary>
+    /// 组包缓存时间/ms
+    /// </summary>
+    [Description("组包缓存时间ms")]
+    public int CacheTimeout { get; set; } = 1000;
+
+    /// <summary>
+    /// 增加FE FE FE FE的报文头部
+    /// </summary>
+    [Description("前导符报文头")]
+    public bool EnableFEHead { get; set; }
+
+    /// <summary>
+    /// 帧前时间ms
+    /// </summary>
+    [Description("帧前时间ms")]
+    public int FrameTime { get; set; }
+    /// <summary>
+    /// 写入需操作员代码
+    /// </summary>
+    [Description("操作员代码")]
+    public string OperCode { get; set; }
+
+    /// <summary>
+    /// 写入密码
+    /// </summary>
+    [Description("写入密码")]
+    public string Password { get; set; }
+
+    /// <summary>
+    /// 通讯地址BCD码，一般应该是12个字符
+    /// </summary>
+    [Description("通讯地址")]
+    public string Station { get; set; }
+    /// <inheritdoc/>
+    public override string GetAddressDescription() => base.GetAddressDescription() + Environment.NewLine;
+    /// <inheritdoc/>
+    public override async Task<OperResult<byte[]>> ReadAsync(string address, int length, CancellationToken token = default)
+    {
+        try
+        {
+            await ConnectAsync(token);
+            var commandResult = DLT645Helper.GetDLT645_2007Command(address, length, (byte)ControlCode.Read, Station);
+            if (commandResult.IsSuccess)
+            {
+                var result = await WaitingClientEx.SendThenResponseAsync(commandResult.Content, TimeOut, token);
+                return (MessageBase)result.RequestInfo;
+            }
+            else
+            {
+                return OperResult.CreateFailedResult<byte[]>(commandResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            return new OperResult<byte[]>(ex);
+        }
+
+    }
+
+    /// <inheritdoc/>
+    public override void SetDataAdapter()
+    {
+        var dataHandleAdapter = new DLT645_2007DataHandleAdapter
+        {
+            EnableFEHead = EnableFEHead
+        };
+        TcpClientEx.SetDataHandlingAdapter(dataHandleAdapter);
+    }
+    /// <inheritdoc/>
+    public override async Task<OperResult> WriteAsync(string address, string value, CancellationToken token = default)
+    {
+
+        try
+        {
+            await ConnectAsync(token);
+            Password ??= string.Empty;
+            OperCode ??= string.Empty;
+            if (Password.Length < 8)
+                Password = Password.PadLeft(8, '0');
+            if (OperCode.Length < 8)
+                OperCode = OperCode.PadLeft(8, '0');
+            var data = DataTransUtil.SpliceArray(Password.ByHexStringToBytes(), OperCode.ByHexStringToBytes());
+            string[] strArray = value.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+            var commandResult = DLT645Helper.GetDLT645_2007Command(address, 1, (byte)ControlCode.Write, Station, data, strArray);
+            if (commandResult.IsSuccess)
+            {
+                var result = await WaitingClientEx.SendThenResponseAsync(commandResult.Content, TimeOut, token);
+                return (MessageBase)result.RequestInfo;
+            }
+            else
+            {
+                return OperResult.CreateFailedResult<byte[]>(commandResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            return new OperResult<byte[]>(ex);
+        }
+
+
+    }
+
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, byte[] value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, uint value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, byte value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, double value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, float value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, long value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, ulong value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, ushort value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, short value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, int value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+
+
+    /// <inheritdoc/>
+    public override Task<OperResult> WriteAsync(string address, bool[] value, CancellationToken token = default) => WriteAsync(address, value.ToString(), token);
+
+
+    #region 其他方法
+
+    /// <summary>
+    /// 广播校时
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public OperResult BroadcastTime(DateTime dateTime, CancellationToken token = default)
+    {
+        try
+        {
+            Connect(token);
+            string str = $"{dateTime.Second:D2}{dateTime.Minute:D2}{dateTime.Hour:D2}{dateTime.Day:D2}{dateTime.Month:D2}{dateTime.Year % 100:D2}";
+            var commandResult = DLT645Helper.GetDLT645_2007Command((byte)ControlCode.BroadcastTime, str.ByHexStringToBytes().Reverse().ToArray(), "999999999999".ByHexStringToBytes());
+            if (commandResult.IsSuccess)
+            {
+                TcpClientEx.Send(commandResult.Content);
+                return OperResult.CreateSuccessResult();
+            }
+            else
+            {
+                return OperResult.CreateFailedResult<string>(commandResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            return new OperResult<string>(ex);
+        }
+
+    }
+
+    /// <summary>
+    /// 冻结
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public async Task<OperResult> FreezeAsync(DateTime dateTime, CancellationToken token = default)
+    {
+        try
+        {
+            await ConnectAsync(token);
+            string str = $"{dateTime.Minute:D2}{dateTime.Hour:D2}{dateTime.Day:D2}{dateTime.Month:D2}";
+            if (Station.IsNullOrEmpty()) Station = string.Empty;
+            if (Station.Length < 12) Station = Station.PadLeft(12, '0');
+            var commandResult = DLT645Helper.GetDLT645_2007Command((byte)ControlCode.Freeze, str.ByHexStringToBytes().Reverse().ToArray(), Station.ByHexStringToBytes().Reverse().ToArray());
+            if (commandResult.IsSuccess)
+            {
+                var result = await WaitingClientEx.SendThenResponseAsync(commandResult.Content, TimeOut, token);
+                var result1 = ((MessageBase)result.RequestInfo);
+                if (result1.IsSuccess)
+                {
+                    return OperResult.CreateSuccessResult();
+
+                }
+                else
+                {
+                    return OperResult.CreateFailedResult<string>(result1);
+                }
+            }
+            else
+            {
+                return OperResult.CreateFailedResult<string>(commandResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            return new OperResult<string>(ex);
+        }
+
+    }
+
+    /// <summary>
+    /// 读取通信地址
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public async Task<OperResult<string>> ReadDeviceStationAsync(CancellationToken token = default)
+    {
+        try
+        {
+            await ConnectAsync(token);
+            var commandResult = DLT645Helper.GetDLT645_2007Command((byte)ControlCode.ReadStation, null, "AAAAAAAAAAAA".ByHexStringToBytes());
+            if (commandResult.IsSuccess)
+            {
+                var result = await WaitingClientEx.SendThenResponseAsync(commandResult.Content, TimeOut, token);
+                var result1 = ((MessageBase)result.RequestInfo);
+                if (result1.IsSuccess)
+                {
+                    var buffer = result1.Content.SelectMiddle(0, 6).BytesAdd(-0x33);
+                    return OperResult.CreateSuccessResult(buffer.Reverse().ToArray().ToHexString());
+                }
+                else
+                {
+                    return OperResult.CreateFailedResult<string>(result1);
+                }
+            }
+            else
+            {
+                return OperResult.CreateFailedResult<string>(commandResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            return new OperResult<string>(ex);
+        }
+
+    }
+
+
+    /// <summary>
+    /// 修改波特率
+    /// </summary>
+    /// <param name="baudRate"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public async Task<OperResult> WriteBaudRateAsync(int baudRate, CancellationToken token = default)
+    {
+        try
+        {
+            await ConnectAsync(token);
+            byte baudRateByte;
+            switch (baudRate)
+            {
+                case 600: baudRateByte = 0x02; break;
+                case 1200: baudRateByte = 0x04; break;
+                case 2400: baudRateByte = 0x08; break;
+                case 4800: baudRateByte = 0x10; break;
+                case 9600: baudRateByte = 0x20; break;
+                case 19200: baudRateByte = 0x40; break;
+                default: return new OperResult<string>($"不支持此波特率:{baudRate}");
+            }
+            if (Station.IsNullOrEmpty()) Station = string.Empty;
+            if (Station.Length < 12) Station = Station.PadLeft(12, '0');
+            var commandResult = DLT645Helper.GetDLT645_2007Command((byte)ControlCode.WriteBaudRate, new byte[] { baudRateByte }, Station.ByHexStringToBytes().Reverse().ToArray());
+            if (commandResult.IsSuccess)
+            {
+                var result = await WaitingClientEx.SendThenResponseAsync(commandResult.Content, TimeOut, token);
+                var result1 = ((MessageBase)result.RequestInfo);
+                if (result1.IsSuccess)
+                {
+                    return OperResult.CreateSuccessResult();
+                }
+                else
+                {
+                    return OperResult.CreateFailedResult<string>(result1);
+                }
+            }
+            else
+            {
+                return OperResult.CreateFailedResult<string>(commandResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            return new OperResult<string>(ex);
+        }
+
+    }
+
+    /// <summary>
+    /// 更新通信地址
+    /// </summary>
+    /// <param name="station"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public async Task<OperResult> WriteDeviceStationAsync(string station, CancellationToken token = default)
+    {
+        try
+        {
+            await ConnectAsync(token);
+            var commandResult = DLT645Helper.GetDLT645_2007Command((byte)ControlCode.WriteStation, station.ByHexStringToBytes().Reverse().ToArray(), "AAAAAAAAAAAA".ByHexStringToBytes());
+            if (commandResult.IsSuccess)
+            {
+                var result = await WaitingClientEx.SendThenResponseAsync(commandResult.Content, TimeOut, token);
+                var result1 = ((MessageBase)result.RequestInfo);
+                if (result1.IsSuccess)
+                {
+                    return OperResult.CreateSuccessResult();
+
+                }
+                else
+                {
+                    return OperResult.CreateFailedResult<string>(result1);
+                }
+            }
+            else
+            {
+                return OperResult.CreateFailedResult<string>(commandResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            return new OperResult<string>(ex);
+        }
+
+    }
+    /// <summary>
+    /// 修改密码
+    /// </summary>
+    /// <param name="level"></param>
+    /// <param name="oldPassword"></param>
+    /// <param name="newPassword"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public async Task<OperResult> WritePasswordAsync(byte level, string oldPassword, string newPassword, CancellationToken token = default)
+    {
+        try
+        {
+            await ConnectAsync(token);
+
+            if (Station.IsNullOrEmpty()) Station = string.Empty;
+            if (Station.Length < 12) Station = Station.PadLeft(12, '0');
+            string str = $"04000C{level:D2}";
+
+            var commandResult = DLT645Helper.GetDLT645_2007Command((byte)ControlCode.WritePassword,
+                str.ByHexStringToBytes().Reverse().ToArray()
+                .SpliceArray(oldPassword.ByHexStringToBytes().Reverse().ToArray())
+                .SpliceArray(newPassword.ByHexStringToBytes().Reverse().ToArray())
+                , Station.ByHexStringToBytes().Reverse().ToArray());
+            if (commandResult.IsSuccess)
+            {
+                var result = await WaitingClientEx.SendThenResponseAsync(commandResult.Content, TimeOut, token);
+                var result1 = ((MessageBase)result.RequestInfo);
+                if (result1.IsSuccess)
+                {
+                    return OperResult.CreateSuccessResult();
+                }
+                else
+                {
+                    return OperResult.CreateFailedResult<string>(result1);
+                }
+            }
+            else
+            {
+                return OperResult.CreateFailedResult<string>(commandResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            return new OperResult<string>(ex);
+        }
+
+    }
+
+
+    #endregion
+
+}
