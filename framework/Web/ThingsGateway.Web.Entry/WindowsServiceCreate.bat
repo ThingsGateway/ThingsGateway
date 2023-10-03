450c15210a70f96eaa354e3df76e101a72082e8b
Kimdiego2098 <2248356998@qq.com>
2023-10-03 19:11:42 +0800

    update windowsService bat

--- framework/Web/ThingsGateway.Web.Entry/WindowsServiceCreate.bat
+++ framework/Web/ThingsGateway.Web.Entry/WindowsServiceCreate.bat
@@ -1,5 +1,5 @@
 cd ..
-sc create ThingsGateway binPath=%~dp0ThingsGateway.Web.Entry.exe start= auto 
+sc create ThingsGateway binPath= %~dp0ThingsGateway.Web.Entry.exe start= auto 
 sc description ThingsGateway "ThingsGateway"
 Net Start ThingsGateway
 pause
