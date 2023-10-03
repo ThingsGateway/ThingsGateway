d3dc4d0c5bde196eba33ecfa740d20fc966ba732
Kimdiego2098 <2248356998@qq.com>
2023-10-04 01:26:44 +0800

    modbusRtu 适配器 过滤干扰头部数据

--- framework/Foundation/ThingsGateway.Foundation.Adapter.Modbus/Modbus/ModbusRtu/ModbusRtuDataHandleAdapter.cs
+++ framework/Foundation/ThingsGateway.Foundation.Adapter.Modbus/Modbus/ModbusRtu/ModbusRtuDataHandleAdapter.cs
@@ -10,6 +10,9 @@
 //------------------------------------------------------------------------------
 #endregion
 
+
+using ThingsGateway.Foundation.Extension.Generic;
+
 namespace ThingsGateway.Foundation.Adapter.Modbus;
 
 /// <summary>
@@ -41,20 +44,43 @@ public class ModbusRtuDataHandleAdapter : ReadWriteDevicesTcpDataHandleAdapter<M
     /// <inheritdoc/>
     protected override FilterResult UnpackResponse(ModbusRtuMessage request, byte[] send, byte[] body, byte[] response)
     {
-        //理想状态检测
-        var result = ModbusHelper.GetModbusRtuData(send, response, Crc16CheckEnable);
-        if (result.IsSuccess)
+        //链路干扰时需剔除前缀中的多于字节，初步按站号+功能码找寻初始字节
+        if (send?.Length > 0)
         {
-            request.ErrorCode = result.ErrorCode;
-            request.Message = result.Message;
-            request.Content = result.Content;
+            int index = -1;
+            for (int i = 0; i < response.Length - 1; i++)
+            {
+                if (response[i] == send[0] && (response[i + 1] == send[1] || response[i + 1] == (send[1] + 0x80)))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index >= 0)
+            {
+                response = response.RemoveBegin(index);
+            }
+
+            //理想状态检测
+            var result = ModbusHelper.GetModbusRtuData(send, response, Crc16CheckEnable);
+            if (result.IsSuccess)
+            {
+                request.ErrorCode = result.ErrorCode;
+                request.Message = result.Message;
+                request.Content = result.Content;
+            }
+            else
+            {
+                request.ErrorCode = result.ErrorCode;
+                request.Message = result.Message;
+            }
+            return result.Content2;
         }
         else
         {
-            request.ErrorCode = result.ErrorCode;
-            request.Message = result.Message;
+            return FilterResult.Success;
         }
-        return result.Content2;
     }
+
 
 }
