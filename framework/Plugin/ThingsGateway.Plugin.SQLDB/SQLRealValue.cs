863beda82ca13840fc73793c8b86517b8faee820
Kimdiego2098 <2248356998@qq.com>
2023-10-16 17:40:17 +0800

    增加关系库存储插件；

---
+++ framework/Plugin/ThingsGateway.Plugin.SQLDB/SQLRealValue.cs
@@ -0,0 +1,51 @@
+﻿#region copyright
+//------------------------------------------------------------------------------
+//  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
+//  此代码版权（除特别声明外的代码）归作者本人Diego所有
+//  源代码使用协议遵循本仓库的开源协议及附加协议
+//  Gitee源代码仓库：https://gitee.com/diego2098/ThingsGateway
+//  Github源代码仓库：https://github.com/kimdiego2098/ThingsGateway
+//  使用文档：https://diego2098.gitee.io/thingsgateway-docs/
+//  QQ群：605534569
+//------------------------------------------------------------------------------
+#endregion
+
+using SqlSugar;
+
+namespace ThingsGateway.Plugin.SQLDB;
+
+[SugarTable(TableDescription = "设备采集实时表")]
+[SugarIndex("index_Name", nameof(SQLHistoryValue.Name), OrderByType.Desc)]
+[SugarIndex("index_DeviceName", nameof(SQLHistoryValue.DeviceName), OrderByType.Desc)]
+public class SQLRealValue
+{
+    [SugarColumn(ColumnDescription = "Id", IsPrimaryKey = true)]
+    public long Id { get; set; }
+    /// <summary>
+    /// 变量名称
+    /// </summary>
+    [SugarColumn(ColumnName = "Name", ColumnDescription = "变量名称")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 设备名称
+    /// </summary>
+    [SugarColumn(ColumnName = "DeviceName", ColumnDescription = "设备名称")]
+    public string DeviceName { get; set; }
+
+    ///<summary>
+    ///实时值
+    ///</summary>
+    [SugarColumn(ColumnName = "Value", ColumnDescription = "实时值")]
+    public string Value { get; set; }
+
+    ///<summary>
+    ///是否在线
+    ///</summary>
+    [SugarColumn(ColumnName = "IsOnline", ColumnDescription = "是否在线 True=在线;False=离线")]
+    public bool IsOnline { get; set; }
+
+    public DateTime CollectTime { get; set; }
+
+}
+
