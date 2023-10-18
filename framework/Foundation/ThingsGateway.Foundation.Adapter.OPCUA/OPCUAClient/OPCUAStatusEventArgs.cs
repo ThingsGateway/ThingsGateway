0e2d58c887d4fb3262d21fa73150c66ca5d8ac6b
youthalan <youthalan@126.com>
2023-10-18 04:32:39 +0000

    补充OPCClient类库事件的缺失文件
    Signed-off-by: youthalan <youthalan@126.com>

--- framework/Foundation/ThingsGateway.Foundation.Adapter.OPCUA/OPCUAClient/OPCUAStatusEventArgs.cs
+++ framework/Foundation/ThingsGateway.Foundation.Adapter.OPCUA/OPCUAClient/OPCUAStatusEventArgs.cs
@@ -1,4 +1,5 @@
-﻿#region copyright
+#region copyright
+
 //------------------------------------------------------------------------------
 //  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
 //  此代码版权（除特别声明外的代码）归作者本人Diego所有
@@ -8,11 +9,43 @@
 //  使用文档：https://diego2098.gitee.io/thingsgateway-docs/
 //  QQ群：605534569
 //------------------------------------------------------------------------------
+
 #endregion
 
 using Opc.Ua;
 
 namespace ThingsGateway.Foundation.Adapter.OPCUA;
+
+/// <summary>
+/// OPC UA的状态更新消息
+/// </summary>
+public class OpcUaStatusEventArgs
+{
+    /// <summary>
+    /// 是否异常
+    /// </summary>
+    public bool Error { get; set; }
+
+    /// <summary>
+    /// 时间
+    /// </summary>
+    public DateTime Time { get; set; }
+
+    /// <summary>
+    /// 文本
+    /// </summary>
+    public string Text { get; set; }
+
+    /// <summary>
+    /// 转化为字符串
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return Error ? "[异常]" : "[正常]" + Time.ToString("  yyyy-MM-dd HH:mm:ss  ") + Text;
+    }
+}
+
 /// <summary>
 /// 读取属性过程中用于描述的
 /// </summary>
@@ -22,6 +55,7 @@ public class OPCNodeAttribute
     /// 属性的名称
     /// </summary>
     public string Name { get; set; }
+
     /// <summary>
     /// 操作结果状态描述
     /// </summary>
@@ -31,10 +65,9 @@ public class OPCNodeAttribute
     /// 属性的类型描述
     /// </summary>
     public string Type { get; set; }
+
     /// <summary>
     /// 属性的值，如果读取错误，返回文本描述
     /// </summary>
     public object Value { get; set; }
-
 }
-
