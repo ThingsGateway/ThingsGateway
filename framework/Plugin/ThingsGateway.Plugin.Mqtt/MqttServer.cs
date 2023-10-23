5d0a2ffae01ec763d15cc94e6657d29f64f6d12d
Kimdiego2098 <2248356998@qq.com>
2023-10-23 20:51:11 +0800

    暂时屏蔽mqttserver 遗留消息错误

--- framework/Plugin/ThingsGateway.Plugin.Mqtt/MqttServer.cs
+++ framework/Plugin/ThingsGateway.Plugin.Mqtt/MqttServer.cs
@@ -256,7 +256,8 @@ public class MqttServer : UpLoadBase
     }
     private void Log_LogMessagePublished(object sender, MqttNetLogMessagePublishedEventArgs e)
     {
-        LogMessage.LogOut(e.LogMessage.Level, e.LogMessage.Source, e.LogMessage.Message, e.LogMessage.Exception);
+        if (e.LogMessage.Exception is not ArgumentNullException)
+            LogMessage.LogOut(e.LogMessage.Level, e.LogMessage.Source, e.LogMessage.Message, e.LogMessage.Exception);
     }
     private void DeviceStatusChange(CollectDeviceRunTime collectDeviceRunTime)
     {
