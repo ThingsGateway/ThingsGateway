a95291d9cde769e856d79ccb2f76d43ba2bc2bae
Kimdiego2098 <2248356998@qq.com>
2023-11-22 14:02:41 +0800

    代码文件编码更改

--- framework/admin/ThingsGateway.Admin.Blazor/Shared/Login.razor.cs
+++ framework/admin/ThingsGateway.Admin.Blazor/Shared/Login.razor.cs
@@ -1,12 +1,12 @@
-#region copyright
+ï»¿#region copyright
 //------------------------------------------------------------------------------
-//  ´Ë´úÂë°æÈ¨ÉùÃ÷ÎªÈ«ÎÄ¼ş¸²¸Ç£¬ÈçÓĞÔ­×÷ÕßÌØ±ğÉùÃ÷£¬»áÔÚÏÂ·½ÊÖ¶¯²¹³ä
-//  ´Ë´úÂë°æÈ¨£¨³ıÌØ±ğÉùÃ÷ÍâµÄ´úÂë£©¹é×÷Õß±¾ÈËDiegoËùÓĞ
-//  Ô´´úÂëÊ¹ÓÃĞ­Òé×ñÑ­±¾²Ö¿âµÄ¿ªÔ´Ğ­Òé¼°¸½¼ÓĞ­Òé
-//  GiteeÔ´´úÂë²Ö¿â£ºhttps://gitee.com/diego2098/ThingsGateway
-//  GithubÔ´´úÂë²Ö¿â£ºhttps://github.com/kimdiego2098/ThingsGateway
-//  Ê¹ÓÃÎÄµµ£ºhttps://diego2098.gitee.io/thingsgateway-docs/
-//  QQÈº£º605534569
+//  æ­¤ä»£ç ç‰ˆæƒå£°æ˜ä¸ºå…¨æ–‡ä»¶è¦†ç›–ï¼Œå¦‚æœ‰åŸä½œè€…ç‰¹åˆ«å£°æ˜ï¼Œä¼šåœ¨ä¸‹æ–¹æ‰‹åŠ¨è¡¥å……
+//  æ­¤ä»£ç ç‰ˆæƒï¼ˆé™¤ç‰¹åˆ«å£°æ˜å¤–çš„ä»£ç ï¼‰å½’ä½œè€…æœ¬äººDiegoæ‰€æœ‰
+//  æºä»£ç ä½¿ç”¨åè®®éµå¾ªæœ¬ä»“åº“çš„å¼€æºåè®®åŠé™„åŠ åè®®
+//  Giteeæºä»£ç ä»“åº“ï¼šhttps://gitee.com/diego2098/ThingsGateway
+//  Githubæºä»£ç ä»“åº“ï¼šhttps://github.com/kimdiego2098/ThingsGateway
+//  ä½¿ç”¨æ–‡æ¡£ï¼šhttps://diego2098.gitee.io/thingsgateway-docs/
+//  QQç¾¤ï¼š605534569
 //------------------------------------------------------------------------------
 #endregion
 
@@ -53,7 +53,7 @@ public partial class Login
         _configTitle = (await _serviceScope.ServiceProvider.GetService<IConfigService>().GetByConfigKeyAsync(ConfigConst.SYS_CONFIGBASEDEFAULT, ConfigConst.CONFIG_TITLE))?.ConfigValue;
         _configRemark = (await _serviceScope.ServiceProvider.GetService<IConfigService>().GetByConfigKeyAsync(ConfigConst.SYS_CONFIGBASEDEFAULT, ConfigConst.CONFIG_REMARK))?.ConfigValue;
         _showCaptcha = (await _serviceScope.ServiceProvider.GetService<IConfigService>().GetByConfigKeyAsync(ConfigConst.SYS_CONFIGBASEDEFAULT, ConfigConst.CONFIG_CAPTCHA_OPEN))?.ConfigValue?.ToBool(false) == true;
-        _welcome = "»¶Ó­Ê¹ÓÃ" + _configTitle + "!";
+        _welcome = "æ¬¢è¿ä½¿ç”¨" + _configTitle + "!";
         await base.OnParametersSetAsync();
     }
 
@@ -101,11 +101,11 @@ public partial class Login
                     await _captcha.RefreshCode();
                 }
 
-                await PopupService.EnqueueSnackbarAsync(new("µÇÂ¼´íÎó" + ": " + ret.Msg.ToString(), AlertTypes.Error));
+                await PopupService.EnqueueSnackbarAsync(new("ç™»å½•é”™è¯¯" + ": " + ret.Msg.ToString(), AlertTypes.Error));
             }
             else
             {
-                await PopupService.EnqueueSnackbarAsync(new("µÇÂ¼³É¹¦", AlertTypes.Success));
+                await PopupService.EnqueueSnackbarAsync(new("ç™»å½•æˆåŠŸ", AlertTypes.Success));
                 await Task.Delay(500);
                 var userId = await _serviceScope.ServiceProvider.GetService<SysUserService>().GetIdByAccountAsync(_loginModel.Account);
                 var data = await _serviceScope.ServiceProvider.GetService<UserCenterService>().GetLoginDefaultRazorAsync(userId);
@@ -123,7 +123,7 @@ public partial class Login
                 await _captcha.RefreshCode();
             }
 
-            await PopupService.EnqueueSnackbarAsync(new("µÇÂ¼´íÎó", AlertTypes.Error));
+            await PopupService.EnqueueSnackbarAsync(new("ç™»å½•é”™è¯¯", AlertTypes.Error));
         }
     }
     private async Task<string> RefreshCode()
