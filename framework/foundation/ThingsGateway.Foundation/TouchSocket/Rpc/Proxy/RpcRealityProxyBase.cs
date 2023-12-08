366aade8b413842236f3558cb3304d0a27adbb90
Kimdiego2098 <2248356998@qq.com>
2023-12-08 17:08:57 +0800

    整理代码文件

--- framework/foundation/ThingsGateway.Foundation/TouchSocket/Rpc/Proxy/RpcRealityProxyBase.cs
+++ framework/foundation/ThingsGateway.Foundation/TouchSocket/Rpc/Proxy/RpcRealityProxyBase.cs
@@ -1,4 +1,18 @@
-﻿#if NET45_OR_GREATER
+﻿#region copyright
+
+//------------------------------------------------------------------------------
+//  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
+//  此代码版权（除特别声明外的代码）归作者本人Diego所有
+//  源代码使用协议遵循本仓库的开源协议及附加协议
+//  Gitee源代码仓库：https://gitee.com/diego2098/ThingsGateway
+//  Github源代码仓库：https://github.com/kimdiego2098/ThingsGateway
+//  使用文档：https://diego2098.gitee.io/thingsgateway-docs/
+//  QQ群：605534569
+//------------------------------------------------------------------------------
+
+#endregion
+
+#if NET45_OR_GREATER
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +25,12 @@ namespace ThingsGateway.Foundation.Rpc
     /// <summary>
     /// RpcRealityProxyBase
     /// </summary>
-    public abstract class RpcRealityProxyBase<T>: RealProxy
+    public abstract class RpcRealityProxyBase<T> : RealProxy
     {
         /// <summary>
         /// RpcRealityProxyBase
         /// </summary>
-        public RpcRealityProxyBase():base(typeof(T))
+        public RpcRealityProxyBase() : base(typeof(T))
         {
 
         }
