6e8b5b431ffe8677b2e24f542d6e912bc4657595
Kimdiego2098 <2248356998@qq.com>
2024-02-04 09:09:29 +0800

    更改种子数据解析方法

--- src/ThingsGateway.Admin.Core/Utils/SeedDataUtil.cs
+++ src/ThingsGateway.Admin.Core/Utils/SeedDataUtil.cs
@@ -14,6 +14,8 @@
 
 using NewLife.Serialization;
 
+using System.Text.RegularExpressions;
+
 namespace ThingsGateway.Admin.Core;
 
 /// <summary>
@@ -30,25 +32,25 @@ public class SeedDataUtil
         if (!string.IsNullOrEmpty(dataString))//如果有内容
         {
             //字段没有数据的替换成null
-            dataString = dataString.Replace("\"\"", "null");
+            dataString = Regex.Replace(dataString, "\\\"[^\"]+?\\\": \\\"\\\"", match => match.Value.Replace("\"\"", "null"));
             //将json字符串转为实体，这里extjson可以正常转换为字符串
             var seedDataRecord1 = dataString.ToJsonEntity<SeedDataRecords<T>>();
 
-            #region 针对导出的json字符串嵌套json字符串如 "DefaultDataScope": "{\"Level\":5,\"ScopeCategory\":\"SCOPE_ALL\",\"ScopeDefineOrgIdList\":[]}"
+            //#region 针对导出的json字符串嵌套json字符串如 "DefaultDataScope": "{\"Level\":5,\"ScopeCategory\":\"SCOPE_ALL\",\"ScopeDefineOrgIdList\":[]}"
 
-            //字符串是\"的替换成"
-            dataString = dataString.Replace("\\\"", "\"");
-            //字符串是\{替换成{
-            dataString = dataString.Replace("\"{", "{");
-            //字符串是}"的替换成}
-            dataString = dataString.Replace("}\"", "}");
-            //将json字符串转为实体,这里extjson会转为null，替换字符串把extjson值变为实体类型而实体类是string类型
-            var seedDataRecord2 = dataString.ToJsonEntity<SeedDataRecords<T>>();
+            ////字符串是\"的替换成"
+            //dataString = dataString.Replace("\\\"", "\"");
+            ////字符串是\{替换成{
+            //dataString = dataString.Replace("\"{", "{");
+            ////字符串是}"的替换成}
+            //dataString = dataString.Replace("}\"", "}");
+            ////将json字符串转为实体,这里extjson会转为null，替换字符串把extjson值变为实体类型而实体类是string类型
+            //var seedDataRecord1 = dataString.ToJsonEntity<SeedDataRecords<T>>();
 
-            #endregion 针对导出的json字符串嵌套json字符串如 "DefaultDataScope": "{\"Level\":5,\"ScopeCategory\":\"SCOPE_ALL\",\"ScopeDefineOrgIdList\":[]}"
+            //#endregion 针对导出的json字符串嵌套json字符串如 "DefaultDataScope": "{\"Level\":5,\"ScopeCategory\":\"SCOPE_ALL\",\"ScopeDefineOrgIdList\":[]}"
 
             //遍历seedDataRecord2
-            for (var i = 0; i < seedDataRecord2.Records.Count; i++)
+            for (var i = 0; i < seedDataRecord1.Records.Count; i++)
             {
                 #region 处理ExtJosn
 
@@ -57,14 +59,14 @@ public class SeedDataUtil
                 if (propertyExtJosn != null)
                 {
                     //获取extjson的值
-                    var extJson = propertyExtJosn.GetValue(seedDataRecord2.Records[i])?.ToString();
+                    var extJson = propertyExtJosn.GetValue(seedDataRecord1.Records[i])?.ToString();
                     // 如果extjson不为空并且包含NullableDictionary表示序列化失败了
                     if (!string.IsNullOrEmpty(extJson) && extJson.Contains("NullableDictionary"))
                     {
                         //设置extjson为seedDataRecord1对应的值
                         extJson = propertyExtJosn.GetValue(seedDataRecord1.Records[i])?.ToString();
                         //seedDataRecord2赋值seedDataRecord1的extjson
-                        propertyExtJosn.SetValue(seedDataRecord2.Records[i], extJson);
+                        propertyExtJosn.SetValue(seedDataRecord1.Records[i], extJson);
                     }
                 }
 
@@ -77,21 +79,21 @@ public class SeedDataUtil
                 if (propertyConfigValue != null)
                 {
                     //获取extjson的值
-                    var configValue = propertyConfigValue.GetValue(seedDataRecord2.Records[i])?.ToString();
+                    var configValue = propertyConfigValue.GetValue(seedDataRecord1.Records[i])?.ToString();
                     // 如果extjson不为空并且包含NullableDictionary表示序列化失败了
                     if (!string.IsNullOrEmpty(configValue) && configValue.Contains("NullableDictionary"))
                     {
                         //设置extjson为seedDataRecord1对应的值
                         configValue = propertyConfigValue.GetValue(seedDataRecord1.Records[i])?.ToString();
                         //seedDataRecord2赋值seedDataRecord1的extjson
-                        propertyConfigValue.SetValue(seedDataRecord2.Records[i], configValue);
+                        propertyConfigValue.SetValue(seedDataRecord1.Records[i], configValue);
                     }
                 }
 
                 #endregion 处理ConfigValue
             }
             //种子数据赋值
-            seedData = seedDataRecord2.Records;
+            seedData = seedDataRecord1.Records;
         }
 
         return seedData;
