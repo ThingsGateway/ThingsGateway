a17000ad6de2da5c4ee903e9e39fbac74c928345
Kimdiego2098 <2248356998@qq.com>
2024-04-18 16:15:53 +0800

    修改判断值变化方法

--- src/ThingsGateway.Gateway.Application/Model/VariableRunTime.cs
+++ src/ThingsGateway.Gateway.Application/Model/VariableRunTime.cs
@@ -1,4 +1,4 @@
-
+﻿
 //------------------------------------------------------------------------------
 //  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
 //  此代码版权（除特别声明外的代码）归作者本人Diego所有
@@ -214,7 +214,7 @@ public class VariableRunTime : Variable, IVariable
                 Type type = data?.GetType();
                 if (typeof(IComparable).IsAssignableFrom(type))
                 {
-                    changed = (data != _value);
+                    changed = !(data.Equals(_value));
                 }
                 else
                 {
