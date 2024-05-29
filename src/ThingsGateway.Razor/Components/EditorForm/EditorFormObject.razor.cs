b912ee3f6f1b6f2ef4a03bdd9c84faffe6a8b9ab
Diego <2248356998@qq.com>
2024-05-29 19:09:38 +0800

    refactor: 删除多余代码

--- src/ThingsGateway.Razor/Components/EditorForm/EditorFormObject.razor.cs
+++ src/ThingsGateway.Razor/Components/EditorForm/EditorFormObject.razor.cs
@@ -12,10 +12,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
-// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-// Website: https://www.blazor.zone or https://argozhang.github.io/
-
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Extensions.Localization;
 
