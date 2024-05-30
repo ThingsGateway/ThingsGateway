e4c4d22f5633bba22502e8ae93c07015e340c2fd
Diego <2248356998@qq.com>
2024-05-30 15:58:32 +0800

    docs: 更新pro用户列表

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -114,7 +114,7 @@ function Gitee() {
             className={"ThingsGateway-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="ThingsGateway-log-number">
-            <div style={{ color: "#723cff" }}>700 +</div>
+            <div style={{ color: "#723cff" }}>750 +</div>
             <span className={isDarkTheme ? " dark" : ""}>Stars</span>
           </div>
         </div>
@@ -126,7 +126,7 @@ function Gitee() {
             className={"ThingsGateway-log-jiao" + (isDarkTheme ? " dark" : "")}
           ></div>
           <div className="ThingsGateway-log-number">
-            <div style={{ color: "#3fbbfe" }}>150 +</div>
+            <div style={{ color: "#3fbbfe" }}>200 +</div>
             <span className={isDarkTheme ? " dark" : ""}>Forks</span>
           </div>
         </div>
