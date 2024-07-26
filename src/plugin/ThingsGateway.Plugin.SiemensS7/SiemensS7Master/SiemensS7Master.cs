8fc36e7b245378fb2a77564c1b906aa506315763
Diego <2248356998@qq.com>
2024-07-26 17:24:26 +0800

    refactor: s7打包后不再主动关闭连接，因为可能对通道多设备产生影响

--- src/plugin/ThingsGateway.Plugin.SiemensS7/SiemensS7Master/SiemensS7Master.cs
+++ src/plugin/ThingsGateway.Plugin.SiemensS7/SiemensS7Master/SiemensS7Master.cs
@@ -91,6 +91,6 @@ public class SiemensS7Master : CollectBase
         {
             return _plc.LoadSourceRead<VariableSourceRead>(deviceVariables, _plc.OnLine ? _plc.PduLength : _driverPropertys.MaxPack, CurrentDevice.IntervalTime);
         }
-        finally { _plc.Channel.Close(); }
+        finally {  }
     }
 }
