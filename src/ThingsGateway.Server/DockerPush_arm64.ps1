e3de01948a45cf2de0916d07f5266b9b5f34f9e3
Diego <2248356998@qq.com>
2024-08-19 13:06:02 +0800

    release：6.0.5.12
    refactor:  更新依赖项
    build:  修改构建脚本
    build:  添加arm64 docker镜像

---
+++ src/ThingsGateway.Server/DockerPush_arm64.ps1
@@ -0,0 +1,3 @@
+docker build -f Dockerfile_arm64 -t registry.cn-shenzhen.aliyuncs.com/thingsgateway/thingsgateway_arm64:latest .
+
+docker push registry.cn-shenzhen.aliyuncs.com/thingsgateway/thingsgateway_arm64
--- src/nugetAndDocker.bat
+++ src/nugetAndDocker.bat
@@ -9,13 +9,30 @@ cd .\ThingsGateway.Server
 set SolutionDir=%~dp0
 
 rem 发布 ThingsGateway.Server 项目
-dotnet publish -c Release --framework net8.0
+dotnet publish -r linux-x64 -c Release --framework net8.0 
+
 
 rem 切换到发布文件夹
-cd bin\Release\net8.0\publish
+cd bin\Release\net8.0\linux-x64\publish
+
 
 rem 执行发布文件夹下的 ps1 文件
 powershell.exe -ExecutionPolicy Bypass -File DockerPush.ps1
+
+
+cd %~dp0
+cd .\ThingsGateway.Server
+
+rem 发布 ThingsGateway.Server 项目
+dotnet publish -r linux-arm64 -c Release --framework net8.0 
+
+
+rem 切换到发布文件夹
+cd bin\Release\net8.0\linux-arm64\publish
+
+rem 执行发布文件夹下的 ps1 文件
+powershell.exe -ExecutionPolicy Bypass -File DockerPush_arm64.ps1
+
 
 rem 切换文件夹
 cd %~dp0
